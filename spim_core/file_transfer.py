b89dbb6affe5957ad3057023b0b11662236d04b9
Micah Woodard <micah.woodard@alleninstitute.org>
2023-03-01 11:22:07 -0800

    changed tiff_transfer to be able to transfer zarr files as well with xcopy command

--- spim_core/file_transfer.py
+++ spim_core/file_transfer.py
@@ -1,6 +1,7 @@
 """File Transfer process in separate class for Win/Linux compatibility."""
 from multiprocessing import Process
 from pathlib import Path
+import subprocess
 import shutil
 import os
 #import subprocess
@@ -15,16 +16,19 @@ class TiffTransfer(Process):
 
     def run(self):
         """Transfer the file from source to dest."""
-        if not os.path.isfile(self.source):
+        if not os.path.isfile(self.source) or os.path.isdir(self.source):
             raise FileNotFoundError(f"{self.source} does not exist.")
         # Transfer the file.
         # print("SKIPPING FILE TRANSFER TO STORAGE")
         print(f"Transferring {self.source} to storage in {self.dest}.")
-        #cmd = subprocess.Popen(f"xcopy {self.source} {self.dest} /i /j")
-        shutil.copy2(self.source, self.dest)
+        parameters = ' /i /j /s /e /t' if os.path.isdir(self.source) else '* /i /j'
+        cmd = subprocess.Popen(f"xcopy {self.source} {self.dest}{parameters}")
+        #shutil.copy2(self.source, self.dest)
         # Delete the old file so we don't run out of local storage.
+        while cmd.poll() is None:
+            # Stalls deleting files until all are copied over
+            not_finished = 'Not finished copying file'
         print(f"Deleting old file at {self.source}.")
-        os.remove(self.source)
+        shutil.rmtree(self.source) if os.path.isdir(self.source) else os.remove(self.source)
         print(f"process finished.")
-
 
