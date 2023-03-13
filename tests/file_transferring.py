0b7d5c5ed21ff2c82f069dae5b7b274b7d23bb10
Micah Woodard <micah.woodard@alleninstitute.org>
2023-03-13 13:07:37 -0700

    changing import names

--- tests/file_transferring.py
+++ tests/file_transferring.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-from spim_core.data_transfer import TiffTransfer
+from spim_core.processes.data_transfer import DataTransfer
 
 if __name__ == "__main__":
 
     source = Path(r'C:\test_dispim\tile_X_0001_Y_0001_Z_0000_cam0.tiff')
     dest = Path(r'Y:\test_dispim\test-ID_2023_02_28\micr\tile_X_0001_Y_0001_Z_0000_cam0.tiff')
-    transfer = TiffTransfer(source, dest)
+    transfer = DataTransfer(source, dest)
     transfer.run()
